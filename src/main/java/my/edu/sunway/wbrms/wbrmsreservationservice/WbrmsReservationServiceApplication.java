35ffa01a1dc4d1d62df11a4c55aae62102ed0cc5
vrnsky <vrnsky@protonmail.ch>
2023-10-07 18:27:00 +0800

    Initial commit with stub implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsreservationservice/WbrmsReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsreservationservice/WbrmsReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
