749503e902bf61ffd58f2011f2b659019e3f030d
vrnsky <vrnsky@protonmail.ch>
2023-10-14 17:19:14 +0800

    CP-003. Automatic deployment
    refs: CP-003

--- src/main/java/my/edu/sunway/wbrms/wbrmsreservationservice/config/CorsConfig.java
+++ src/main/java/my/edu/sunway/wbrms/wbrmsreservationservice/config/CorsConfig.java
@@ -3,30 +3,25 @@ package my.edu.sunway.wbrms.wbrmsreservationservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Collections;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
-    private String allowedOriginPattern = "*";
-    private String allowedOrigin = "*";
-    private String allowedHeader = "*";
-    private String allowedMethod = "*";
-    private String corsPattern = "/**";
-
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(Collections.singletonList(allowedOriginPattern));
-        config.addAllowedOrigin(allowedOrigin);
-        config.addAllowedHeader(allowedHeader);
-        config.addAllowedMethod(allowedMethod);
-        source.registerCorsConfiguration(corsPattern, config);
-        return new CorsFilter(source);
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
