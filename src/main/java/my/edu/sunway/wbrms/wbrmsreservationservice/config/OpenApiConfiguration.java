4b8f8eda41e13695edb61be85ccf9015171df76f
vrnsky <vrnsky@protonmail.ch>
2023-10-14 17:48:50 +0800

    CP-003. Changes due to CORS
    refs: CP-003

--- src/main/java/my/edu/sunway/wbrms/wbrmsreservationservice/config/OpenApiConfiguration.java
+++ src/main/java/my/edu/sunway/wbrms/wbrmsreservationservice/config/OpenApiConfiguration.java
@@ -3,9 +3,11 @@ package my.edu.sunway.wbrms.wbrmsreservationservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Configuration
 public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
